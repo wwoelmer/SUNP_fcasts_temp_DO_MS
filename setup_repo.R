5a70ec323e62761c0111b93737d121e15e42eb63
wwoel <wwoelmer@vt.edu>
2021-06-17 16:32:56 -0400

    documentation

--- setup_repo.R
+++ setup_repo.R
@@ -12,7 +12,7 @@ setwd(config$file_path$data_directory)
 system("git clone -b sunp-buoy-data --depth 1 https://github.com/FLARE-forecast/SUNP-data.git buoy-data")
 
 ############## download historical data
-# buoy data from 2007 onward
+# buoy temp and DO data from 2007 onward
 data  <-  "https://pasta.lternet.edu/package/data/eml/edi/499/2/1f903796efc8d79e263a549f8b5aa8a6" # URL from EDI: https://portal.edirepository.org/nis/codeGeneration?packageId=edi.499.2&statisticalFileType=r
 destination <- config$file_path$data_directory # some location on your computer
 try(download.file(data,destfile = paste0(destination, '/hist-data/hist_buoy_temp.csv'),method="curl"))
