5d323e24adcf0929de9822dc574404f70f20875d
wwoel <wwoelmer@vt.edu>
2021-06-22 12:24:25 -0400

    add loop to check if noaa data exists in local directory

--- 01_get_data.R
+++ 01_get_data.R
@@ -13,17 +13,38 @@ config_obs <- yaml::read_yaml(file.path(lake_directory,"configuration", "observa
 # download buoy data, water quality and met
 setwd(file.path(config$file_path$data_directory, config_obs$realtime_insitu_location))
 system("git pull")
+setwd("C:/Users/wwoel/Desktop/SUNP-forecast")
 
 # download NOAA data
 # source a function here
 
 source(paste0(config$file_path$script_directory, "/noaa_download_s3.R"))
 
-noaa_download_s3(siteID = 'fcre',
-                 date = "2021-06-16",
-                 cycle = '00',
-                 noaa_horizon = 16,
-                 noaa_directory = config$file_path$noaa_directory)
+# set a start and end date for NOAA forecasts and check which days are not available in local NOAA directory
+dates <- seq.Date(as.Date('2021-05-22'), as.Date(Sys.Date()), by = 'day')
+download_dates <- array(NA)
+path_dates <- array(NA, length(dates))
+for (i in 1:length(dates)) {
+  path_dates[i] <- paste0(config$file_path$noaa_directory, "drivers/noaa-point/NOAAGEFS_1hr/sunp/", dates[i])
+  if(file.exists(path_dates[i])){
+    print(paste0(dates[i], ' already downloaded'))
+  }else{
+    download_dates <- c(download_dates, dates[i])
+  }
+}
+
+download_dates <- na.omit(download_dates)
+download_dates <- as.Date(download_dates, origin = '1970-01-01')
 
 
+for (i in 1:length(download_dates)) {
+  noaa_download_s3(siteID = 'sunp',
+                   date = download_dates[i],
+                   cycle = '00',
+                   noaa_horizon = 16,
+                   noaa_directory = config$file_path$noaa_directory)
+  
+}
+
+# noaa-point is 16 day
 
