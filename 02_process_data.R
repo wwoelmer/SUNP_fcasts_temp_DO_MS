def1eb19bf9deb416e24e7ab8c9373dac83a77b1
wwoel <wwoelmer@vt.edu>
2021-07-21 15:20:06 -0400

    remove config readin

--- 02_process_data.R
+++ 02_process_data.R
@@ -1,22 +1,22 @@
-
-lake_directory <- getwd()
-config <- yaml::read_yaml(file.path(lake_directory, "configuration", "FLAREr", "configure_flare.yml"))
-
-# Set working directories for your system
-config$file_path$qaqc_data_directory <- file.path(lake_directory, "data_processed")
-config$file_path$data_directory <- file.path(lake_directory, "data_raw")
-config$file_path$noaa_directory <- file.path(lake_directory, "forecasted_drivers")
-
-# set up run config settings
-run_config <- yaml::read_yaml(file.path(lake_directory,"configuration", "FLAREr", "configure_run.yml"))
-config$run_config <- run_config
-
-# set forecast model info
-if(config$run_config$forecast_horizon==16){
-  config$met$forecast_met_model <-'noaa-point/NOAAGEFS_1hr'
-}else if(config$run_config$forecast_horizon==35){
-  config$met$forecast_met_model <- 'noaa/NOAAGEFS_1hr'
-}
+# 
+# lake_directory <- getwd()
+# config <- yaml::read_yaml(file.path(lake_directory, "configuration", "FLAREr", "configure_flare.yml"))
+# 
+# # Set working directories for your system
+ config$file_path$qaqc_data_directory <- file.path(lake_directory, "data_processed")
+ config$file_path$data_directory <- file.path(lake_directory, "data_raw")
+ config$file_path$noaa_directory <- file.path(lake_directory, "forecasted_drivers")
+# 
+# # set up run config settings
+# run_config <- yaml::read_yaml(file.path(lake_directory,"configuration", "FLAREr", "configure_run.yml"))
+# config$run_config <- run_config
+# 
+# # set forecast model info
+ if(config$run_config$forecast_horizon==16){
+   config$met$forecast_met_model <-'noaa-point/NOAAGEFS_1hr'
+ }else if(config$run_config$forecast_horizon==35){
+   config$met$forecast_met_model <- 'noaa/NOAAGEFS_1hr'
+ }
 
 
 library(tidyverse)
@@ -24,12 +24,12 @@ library(lubridate)
 
 
 # get NOAA met forecasts and stack first day to use as met 'obs'
-source(file.path(lake_directory, "R", "/stack_noaa_forecasts.R"))
-dates <- seq.Date(as.Date('2021-05-23'), as.Date('2021-06-24'), by = 'day') # cycle through historical dates 
+source(file.path(lake_directory, "R", "stack_noaa_forecasts_cycle_loop.R"))
+dates <- seq.Date(as.Date('2021-05-26'), as.Date(config$run_config$forecast_start_datetime), by = 'day') # cycle through historical dates 
 cycle <- '00'
 outfile <- config$file_path$qaqc_data_directory
 stack_noaa_forecasts(dates = dates, 
-                     cycle = cycle, 
+                     #cycle = cycle, 
                      outfile = outfile, 
                      config = config)
 
