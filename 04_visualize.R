11bd267d3e7cda507d3e14c0af82c3ca03fcc8d5
wwoel <wwoelmer@vt.edu>
2021-07-21 15:20:57 -0400

    add new simple plot function

--- 04_visualize.R
+++ 04_visualize.R
@@ -4,13 +4,18 @@
 #file_name <- saved_file
 FLAREr::plotting_general(file_name = saved_file,qaqc_data_directory = config$file_path$qaqc_data_directory, ncore = 2)
 
-visualization_location <- file.path(lake_directory,"visualization")
-source(paste0(visualization_location,"/manager_plot.R"))
+source(file.path(lake_directory,"R/simple_plot.R"))
 
-#saved_file <- "/Users/quinn/Dropbox (VTFRS)/Research/SSC_forecasting/run_flare_package/wrr_runs_obs/wrr_runs_H_2019_03_14_2019_03_15_F_16_20201124T085027.nc"
-#qaqc_data_location <- "/Users/quinn/Dropbox/Research/SSC_forecasting/FLARE_package/flare_lake_examples/fcre/qaqc_data/"
+forecast_file_name <- saved_file
+output_file_name <- paste0(config$file_path$forecast_output_directory, "/", config$run_config$sim_name, "_", lubridate::date(config$run_config$forecast_start_datetime), "_", config$run_config$forecast_horizon, '_day_forecast_simple_plot' )
+qaqc_data_directory <- config$file_path$qaqc_data_directory
+focal_depths_plotting <- c('0.1', '5', '10')
+highlight_date <- as.Date('2021-07-17')
 
-manager_plot(file_name = saved_file,
-             qaqc_location = qaqc_data_location,
-             focal_depths = c(1, 5, 8))
+simple_plot(forecast_file_name,
+            output_file_name,
+            qaqc_data_directory,
+            focal_depths_plotting,
+            highlight_date = highlight_date)
+
 
