6ef5ddf0dfc3d45296eae8ef6a2f99efe4904c90
wwoel <wwoelmer@vt.edu>
2021-07-21 15:22:14 -0400

    remove catch for if forecast files < 1

--- R/stack_noaa_forecasts_cycle_loop.R
+++ R/stack_noaa_forecasts_cycle_loop.R
@@ -47,43 +47,43 @@ stack_noaa_forecasts <- function(dates, # list of dates you have NOAA GEFS .nc f
     
     cycle <- list.files(file.path(config$file_path$noaa_directory, config$met$forecast_met_model, config$location$site_id, dates[k]))
     
-    if(length(forecast_files) < 1){
-      noaa_obs_out <- na.omit(noaa_obs_out)
-      #write.csv(noaa_obs_out, paste0(outfile, '/NOAA_GEFS_mean_ens_', dates[1], '-', dates[k-1], '.csv'), row.names = FALSE)
-      # write to nc
-      start_time <- min(noaa_obs_out$time)
-      end_time <- max(noaa_obs_out$time)
-      
-      data <- noaa_obs_out %>%
-        dplyr::select(-time)
-      
-      diff_time <- as.numeric(difftime(noaa_obs_out$time, noaa_obs_out$time[1], units = "hours"))
-      
-      cf_var_names <- names(data)
-      
-      time_dim <- ncdf4::ncdim_def(name="time",
-                                   units = paste("hours since", format(start_time, "%Y-%m-%d %H:%M")),
-                                   diff_time, #GEFS forecast starts 5 hours from start time
-                                   create_dimvar = TRUE)
-      lat_dim <- ncdf4::ncdim_def("latitude", "degree_north", lat, create_dimvar = TRUE)
-      lon_dim <- ncdf4::ncdim_def("longitude", "degree_east", lon, create_dimvar = TRUE)
-      
-      dimensions_list <- list(time_dim, lat_dim, lon_dim)
-      
-      nc_var_list <- list()
-      for (i in 1:length(cf_var_names)) { #Each ensemble member will have data on each variable stored in their respective file.
-        nc_var_list[[i]] <- ncdf4::ncvar_def(cf_var_names[i], cf_units[i], dimensions_list, missval=NaN)
-      }
-      
-      nc_flptr <- ncdf4::nc_create(output_file, nc_var_list, verbose = FALSE, )
-      
-      #For each variable associated with that ensemble
-      for (j in 1:ncol(data)) {
-        # "j" is the variable number.  "i" is the ensemble number. Remember that each row represents an ensemble
-        ncdf4::ncvar_put(nc_flptr, nc_var_list[[j]], unlist(data[,j]))
-      }
-      ncdf4::nc_close(nc_flptr)  #Write to the disk/storage
-    }
+    # if(length(forecast_files) < 1){
+    #   noaa_obs_out <- na.omit(noaa_obs_out)
+    #   #write.csv(noaa_obs_out, paste0(outfile, '/NOAA_GEFS_mean_ens_', dates[1], '-', dates[k-1], '.csv'), row.names = FALSE)
+    #   # write to nc
+    #   start_time <- min(noaa_obs_out$time)
+    #   end_time <- max(noaa_obs_out$time)
+    #   
+    #   data <- noaa_obs_out %>%
+    #     dplyr::select(-time)
+    #   
+    #   diff_time <- as.numeric(difftime(noaa_obs_out$time, noaa_obs_out$time[1], units = "hours"))
+    #   
+    #   cf_var_names <- names(data)
+    #   
+    #   time_dim <- ncdf4::ncdim_def(name="time",
+    #                                units = paste("hours since", format(start_time, "%Y-%m-%d %H:%M")),
+    #                                diff_time, #GEFS forecast starts 5 hours from start time
+    #                                create_dimvar = TRUE)
+    #   lat_dim <- ncdf4::ncdim_def("latitude", "degree_north", lat, create_dimvar = TRUE)
+    #   lon_dim <- ncdf4::ncdim_def("longitude", "degree_east", lon, create_dimvar = TRUE)
+    #   
+    #   dimensions_list <- list(time_dim, lat_dim, lon_dim)
+    #   
+    #   nc_var_list <- list()
+    #   for (i in 1:length(cf_var_names)) { #Each ensemble member will have data on each variable stored in their respective file.
+    #     nc_var_list[[i]] <- ncdf4::ncvar_def(cf_var_names[i], cf_units[i], dimensions_list, missval=NaN)
+    #   }
+    #   
+    #   nc_flptr <- ncdf4::nc_create(output_file, nc_var_list, verbose = FALSE, )
+    #   
+    #   #For each variable associated with that ensemble
+    #   for (j in 1:ncol(data)) {
+    #     # "j" is the variable number.  "i" is the ensemble number. Remember that each row represents an ensemble
+    #     ncdf4::ncvar_put(nc_flptr, nc_var_list[[j]], unlist(data[,j]))
+    #   }
+    #   ncdf4::nc_close(nc_flptr)  #Write to the disk/storage
+    # }
     
       daily_noaa <- data.frame(matrix(ncol = length(cf_met_vars) + 3, nrow = 0))
       colnames(daily_noaa) <- c('time', cf_met_vars, 'ens', 'cycle')
