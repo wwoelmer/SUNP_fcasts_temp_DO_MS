1ea2c257147fa16bcb1f2022a382d6f9202fc628
wwoel <wwoelmer@vt.edu>
2021-07-30 11:19:44 -0400

    fix cycle file naming convention

--- R/noaa_download_s3.R
+++ R/noaa_download_s3.R
@@ -10,7 +10,7 @@ noaa_download_s3 <- function(siteID, # LOWERCASE e.g. sunp
                              ){
 
   Sys.setenv("AWS_S3_ENDPOINT" = "tacc.jetstream-cloud.org:8080/")
-  
+  later_cycles <- c('06', '12', '18')
   # currently not able to get 35 day forecasts downloaded but the setup is here
   if(noaa_horizon == 16) {
     end_date <- as.Date(date) + 16
@@ -23,9 +23,13 @@ noaa_download_s3 <- function(siteID, # LOWERCASE e.g. sunp
   
   
   ens <- formatC(seq(0, 30), width = 2, flag = 0)
-  file_names <- file.path(prefix, siteID, date, cycle, paste0("NOAAGEFS_", noaa_hour, "hr_", siteID, "_", date, "T", cycle, "_", end_date, "T", cycle, "_ens", ens, ".nc"))
+  file_names <- file.path(prefix, siteID, date, cycle, paste0("NOAAGEFS_", noaa_hour, "hr_", siteID, "_", date, "T00", "_", end_date, "T", cycle, "_ens", ens, ".nc"))
   if(noaa_horizon == 35){
-    file_names[1] <- file.path(prefix, siteID, date, cycle, paste0("NOAAGEFS_", noaa_hour, "hr_", siteID, "_", date, "T00_", end_date_00, "T00_ens", ens, ".nc"))
+    file_names[1] <- file.path(prefix, siteID, date, cycle, paste0("NOAAGEFS_", noaa_hour, "hr_", siteID, "_", date, "T00", "_",  end_date_00, "T", cycle, "_ens", ens, ".nc"))
+  }
+  if(cycle %in% later_cycles){
+    file_names <- file.path(prefix, siteID, date, cycle, paste0("NOAAGEFS_", noaa_hour, "hr_", siteID, "_", date, "T00", "_", end_date_00, "T", cycle, "_ens", ens, ".nc"))
+    
   }
   
   #Download a specific file from the server and save it locally (in this example, "localfile.nc"):
