aa047b3d7b878f66de0efa16d85ba6ae5455ae0e
rqthomas <rqthomas@vt.edu>
2021-11-05 10:23:05 +0000

    cleaning of generate targets

--- 01_generate_targets.R
+++ 01_generate_targets.R
@@ -8,8 +8,9 @@ lake_directory <- here::here()
 
 s3_mode <- TRUE
 
-files.sources <- list.files(file.path(lake_directory, "R"), full.names = TRUE)
-sapply(files.sources, source)
+source(file.path(lake_directory, "R", "s3_functions.R"))
+source(file.path(lake_directory, "R", "average_stacked_forecasts.R"))
+source(file.path(lake_directory, "R", "insitu_qaqc.R"))
 
 if(file.exists("~/.aws")){
   warning(paste("Detected existing AWS credentials file in ~/.aws,",
@@ -17,7 +18,7 @@ if(file.exists("~/.aws")){
 }
 
 Sys.setenv("AWS_DEFAULT_REGION" = "s3",
-           "AWS_S3_ENDPOINT" = "flare-forecast")
+           "AWS_S3_ENDPOINT" = "flare-forecast.org")
 
 configuration_file <- "configure_flare.yml"
 run_config <- yaml::read_yaml(file.path(lake_directory,"configuration","FLAREr","configure_run.yml"))
@@ -39,6 +40,10 @@ if(!dir.exists(file.path(lake_directory, "data_raw", config_obs$realtime_insitu_
   system("git pull")
 }
 
+if(!dir.exists(dir.create(file.path(config$file_path$data_directory, "hist-data")))){
+  dir.create(file.path(config$file_path$data_directory, "hist-data"))
+}
+
 if(!file.exists(file.path(config$file_path$data_directory, "hist-data", "hist_buoy_temp.csv"))){
   data  <-  "https://pasta.lternet.edu/package/data/eml/edi/499/2/1f903796efc8d79e263a549f8b5aa8a6" # URL from EDI: https://portal.edirepository.org/nis/codeGeneration?packageId=edi.499.2&statisticalFileType=r
   try(download.file(data, destfile = file.path(config$file_path$data_directory, "hist-data", "hist_buoy_temp.csv"), 
@@ -54,7 +59,7 @@ if(!file.exists(file.path(config$file_path$data_directory, "hist-data", "hist_bu
 download_s3_objects(lake_directory, bucket = "drivers", prefix = file.path("noaa", "NOAAGEFS_1hr_stacked", forecast_site))
 
 # format the stacked forecasts for input into flare
-average_stacked_forecasts(forecast_dates = seq.Date(as.Date(config$run_config$start_datetime), as.Date(run_config$forecast_start_datetime), by = 'day'), # cycle through historical dates
+average_stacked_forecasts(forecast_dates = seq.Date(as_date(run_config$start_datetime), as_date(run_config$forecast_start_datetime), by = 'day'), # cycle through historical dates
                           site = forecast_site, #four digit name in lowercase
                           noaa_stacked_directory = file.path(lake_directory, "drivers", "noaa", "NOAAGEFS_1hr_stacked"),
                           output_directory = file.path(lake_directory, "data_processed"),
@@ -62,11 +67,8 @@ average_stacked_forecasts(forecast_dates = seq.Date(as.Date(config$run_config$st
                           noaa_hour = 1)
 
 # QAQC insitu buoy data
-realtime_file <- file.path(config$file_path$data_directory, config_obs$insitu_obs_fname[1])
-hist_file <- file.path(config$file_path$data_directory, config_obs$insitu_obs_fname[2])
-
-insitu_qaqc(realtime_file = realtime_file,
-            hist_file = hist_file,
+insitu_qaqc(realtime_file = file.path(config$file_path$data_directory, config_obs$insitu_obs_fname[1]),
+            hist_file =  file.path(config$file_path$data_directory, config_obs$insitu_obs_fname[2]),
             maintenance_url = "https://docs.google.com/spreadsheets/d/1IfVUlxOjG85S55vhmrorzF5FQfpmCN2MROA_ttEEiws/edit?usp=sharing",
             variables = "temperature",
             config = config)
