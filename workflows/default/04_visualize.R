e4ae5d8fd2e4021d41fee023feac9d1aeada501e
wwoel <wwoelmer@vt.edu>
2022-07-01 13:28:07 -0400

    change format of simple pdf file name

--- workflows/default/04_visualize.R
+++ workflows/default/04_visualize.R
@@ -18,7 +18,7 @@ config <- FLAREr::get_restart_file(config, lake_directory)
 
 FLAREr::get_targets(lake_directory, config)
 
-pdf_file <- FLAREr::plotting_general_2(file_name =  saved_file, #"C:/Users/wwoel/Desktop/SUNP-forecast-code/forecasts/sunp/sunp-2022-05-22-sunp_V1.nc", #config$run_config$restart_file,
+pdf_file <- FLAREr::plotting_general_2(file_name =  config$run_config$restart_file,
                                        target_file = file.path(config$file_path$qaqc_data_directory, paste0(config$location$site_id, "-targets-insitu.csv")))
 
 if(config$run_config$use_s3){
@@ -30,8 +30,8 @@ if(config$run_config$use_s3){
 
 source(file.path(lake_directory,"R/simple_plot.R"))
 
-forecast_file_name <- saved_file #"C:/Users/wwoel/Desktop/SUNP-forecast-code/forecasts/sunp/sunp-2022-05-22-sunp_V1.nc" #config$run_config$restart_file
-output_file_name <- paste0(config$file_path$forecast_output_directory, "/", config$run_config$sim_name, "_", lubridate::date(config$run_config$forecast_start_datetime), "_", config$run_config$forecast_horizon, '_day_forecast_simple_plot' )
+forecast_file_name <- config$run_config$restart_file
+output_file_name <- paste0(config$file_path$forecast_output_directory, "/", config$run_config$sim_name, "_", config$run_config$forecast_horizon, 'day_simple_plot_', lubridate::date(config$run_config$forecast_start_datetime))
 qaqc_data_directory <- config$file_path$qaqc_data_directory
 focal_depths_plotting <- c('0.1', '5', '10')
 highlight_date <- Sys.Date()
