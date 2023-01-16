04de87df6244d8f374b571a2bb7a30a769a0dafe
Whitney Marie Woelmer <wwoelmer@tc002.cm.cluster>
2023-01-16 13:32:15 -0500

    fix indexing, add stacked bar plot

--- workflows/UC_analysis/analyze_UC.R
+++ workflows/UC_analysis/analyze_UC.R
@@ -1,15 +1,18 @@
 library(tidyverse)
+#install.packages('ggpubr')
+library(ggpubr)
+
 lake_directory <- here::here()
 
 folders <- c('initial_condition', 'observation', 'parameter', 'process', 'weather')
 forecast_folder <- file.path(lake_directory, 'forecasts/sunp/', folders[1])
 files1 <- list.files(path = forecast_folder, pattern = "*.csv.gz")
-uncert <- read.csv(paste0(forecast_folder, "/", files1[3]))
+uncert <- read.csv(paste0(forecast_folder, "/", files1[2]))
 
 # some subsetting
 vars <- c('temperature', 'oxygen')
 depths <- c(1.0, 10.0)
-horizons <- c(1, 7, 20, 35)
+horizons <- c(1:35)
 uncert <- uncert %>% 
   filter(variable %in% vars,
          forecast > 0,
@@ -17,13 +20,14 @@ uncert <- uncert %>%
   mutate(horizon = as.POSIXct(datetime) - as.POSIXct(reference_datetime)) %>% 
   filter(horizon %in% horizons) 
   
-starting_index <- 3
+starting_index <- 1
+# j <- starting_index
 for(j in starting_index:length(folders)){
   forecast_folder <- file.path(lake_directory, 'forecasts/sunp', folders[j])
   print(folders[j])
   # the year's forecasts with all uncertainties 'on' in the forecast output
   myfiles <- list.files(path = forecast_folder, pattern = "*.csv.gz")
-  dataset <- read.csv(paste0(forecast_folder, "/", myfiles[3]))
+  dataset <- read.csv(paste0(forecast_folder, "/", myfiles[2]))
   
   dataset <- dataset %>% 
     filter(variable %in% vars,
@@ -33,7 +37,7 @@ for(j in starting_index:length(folders)){
     filter(horizon %in% horizons) 
   
   # read in files
-  for (i in 4:length(myfiles)) {
+  for (i in 3:length(myfiles)) {
     print(i)
     temp <- read.csv(paste0(forecast_folder, "/", myfiles[i]))
     temp <- temp %>% 
@@ -67,10 +71,76 @@ for(j in starting_index:length(folders)){
 
 write.csv(uncert, file.path(lake_directory, 'all_uncertainty.csv'), row.names = FALSE)
 
+##############################################################
+# now look at all forecasts over time at different horizons
 uncert <- read.csv(file.path(lake_directory, 'all_uncertainty.csv'))
 
-# plot one day's forecast across horizons
-fcast_date <- as.POSIXct('2022-06-20')
+uncert_sum <- uncert %>% 
+  select(datetime, depth, model_id, variable, parameter, prediction, horizon) %>% 
+  group_by(model_id, variable, depth, datetime, horizon) %>% 
+  mutate(variance = var(prediction)) %>% 
+  mutate(mean_pred = mean(prediction)) %>% 
+  distinct(variable, depth, datetime, horizon, .keep_all = TRUE) %>% 
+  select(-prediction, -parameter)
+
+uncert_prop <- uncert_sum %>% 
+  group_by(variable, depth, datetime, horizon) %>% 
+  mutate(total_var = sum(variance)) %>% 
+  mutate(var_prop = variance/total_var)
+
+# aggregate over each horizon 
+# create dataframe to write into inside the loop (nrow = # of horizons, ncol = # of UC modes + 1)
+
+uncert_mean <- uncert_prop %>% 
+  group_by(horizon, variable, depth, model_id) %>% 
+  mutate(mean_prop = mean(var_prop)) %>% 
+  distinct(horizon, variable, depth, model_id, .keep_all = TRUE)
+
+################
+# make some figs
+# stacked bar plot over all horizons
+ggplot(uncert_mean, aes(x = horizon, y = mean_prop, fill = model_id)) +
+  geom_bar(stat = 'identity', position= 'stack') +
+  facet_grid(cols = vars(variable), rows = vars(depth)) 
+
+# subset down to a few horizons
+horizon_sub <- c(1, 7, 20, 35)
+uncert_prop2 <- uncert_prop %>% 
+  filter(horizon %in% horizon_sub)
+
+ggplot(uncert_prop2[uncert_prop2$depth==1,], 
+       aes(x = as.POSIXct(datetime), y = var_prop, fill = model_id)) +
+  geom_area() +
+  facet_grid(cols = vars(variable), rows = vars(horizon)) +
+  ggtitle("1.0m")
+
+ggplot(uncert_prop2[uncert_prop2$depth==10,], 
+       aes(x = as.POSIXct(datetime), y = var_prop, fill = model_id)) +
+  geom_area() +
+  facet_grid(cols = vars(variable), rows = vars(horizon)) +
+  ggtitle("10.0m")
+
+t <- ggplot(uncert_prop2[uncert_prop2$variable=='temperature',], 
+            aes(x = as.POSIXct(datetime), y = variance, group = model_id, color = model_id)) +
+  geom_line() +
+  facet_grid(cols = vars(depth), rows = vars(horizon), scales = 'free') +
+  ggtitle('temperature') +
+  xlab('Date')
+
+o <- ggplot(uncert_prop2[uncert_prop2$variable=='oxygen',], 
+            aes(x = as.POSIXct(datetime), y = variance, group = model_id, color = model_id)) +
+  geom_line() +
+  facet_grid(cols = vars(depth), rows = vars(horizon), scales = 'free') +
+  ggtitle('oxygen') +
+  xlab('Date')
+
+ggarrange(o, t, common.legend = TRUE)
+
+
+
+#####################################################################################
+# plot just one day's forecast across horizons
+fcast_date <- as.POSIXct('2022-06-08')
 folders
 fcast <- read.csv(file.path(lake_directory, 'forecasts/sunp', folders[1], paste0("sunp-", fcast_date, "-",  folders[1], ".csv.gz")))
 for(m in 2:length(folders)){
@@ -163,53 +233,6 @@ ggplot(area[area$variable=='oxygen',], aes(x = as.Date(datetime), y = model_mean
   ggtitle('oxygen')
 
 
-uncert_sum <- uncert %>% 
-  select(datetime, depth, model_id, variable, parameter, prediction, horizon) %>% 
-  group_by(model_id, variable, depth, datetime, horizon) %>% 
-  mutate(variance = var(prediction)) %>% 
-  mutate(mean_pred = mean(prediction)) %>% 
-  distinct(variable, depth, datetime, horizon, .keep_all = TRUE) %>% 
-  select(-prediction, -parameter)
-
-uncert_prop <- uncert_sum %>% 
-  group_by(variable, depth, datetime, horizon) %>% 
-  mutate(total_var = sum(variance)) %>% 
-  mutate(var_prop = variance/total_var)
-
-temp_1m <- uncert_prop %>% 
-  filter(variable=='temperature', 
-         depth=='1')
-  
-ggplot(uncert_prop[uncert_prop$depth==1,], 
-       aes(x = as.POSIXct(datetime), y = var_prop, fill = model_id)) +
-  geom_area() +
-  facet_grid(cols = vars(variable), rows = vars(horizon))
-
-ggplot(uncert_prop[uncert_prop$depth==1,], 
-       aes(x = as.POSIXct(datetime), y = var_prop, fill = model_id)) +
-  geom_area() +
-  facet_grid(cols = vars(variable), rows = vars(horizon)) +
-  ggtitle("10.0m")
-
-t <- ggplot(uncert_prop[uncert_prop$variable=='temperature',], 
-       aes(x = as.POSIXct(datetime), y = variance, group = model_id, color = model_id)) +
-  geom_line() +
-  facet_wrap(~depth) +
-  ggtitle('temperature') +
-  xlab('Date')
-  
-o <- ggplot(uncert_prop[uncert_prop$variable=='oxygen',], 
-       aes(x = as.POSIXct(datetime), y = variance, group = model_id, color = model_id)) +
-  geom_line() +
-  facet_wrap(~depth) +
-  ggtitle('oxygen') +
-  xlab('Date')
-
-#install.packages('ggpubr')
-library(ggpubr)
-ggarrange(t, o, common.legend = TRUE)
-
-
 ##############################################################################################
 # visualize the normal run (all_UC)
 forecast_folder <- file.path(lake_directory, 'forecasts/sunp/all_UC')
@@ -229,7 +252,7 @@ dataset <- dataset %>%
   filter(horizon==1)
 
 # read in files
-for (i in 4:length(myfiles)) {
+for (i in 2:length(myfiles)) {
   print(i)
   temp <- read.csv(paste0(forecast_folder, "/", myfiles[i]))
   temp <- temp %>% 
