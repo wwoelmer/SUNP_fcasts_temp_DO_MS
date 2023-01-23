31bed5bef223a5c356f646ad3b070a6de9c1e80c
Whitney Marie Woelmer <wwoelmer@tc073.cm.cluster>
2023-01-23 11:24:17 -0500

    fix indexing

--- workflows/UC_analysis/compare_to_obs.R
+++ workflows/UC_analysis/compare_to_obs.R
@@ -32,7 +32,7 @@ for(j in starting_index:length(folders)){
   print(folders[j])
 
   files1 <- list.files(path = score_folder, pattern = "*.parquet")
-  dataset <- read_parquet(file.path(score_folder, files1[3]))
+  dataset <- read_parquet(file.path(score_folder, files1[2]))
   
   dataset <- dataset %>% 
     separate(site_id, into = c("site", "depth"), sep = "-") %>% 
@@ -40,7 +40,7 @@ for(j in starting_index:length(folders)){
            depth %in% depths)  
   
   # read in files
-  for (i in 4:length(files1)) {
+  for (i in 3:length(files1)) {
     print(i)
     temp <- read_parquet(paste0(score_folder, "/", files1[i]))
     temp <- temp %>% 
@@ -58,6 +58,24 @@ write.csv(f, file.path(lake_directory, 'all_obs_fcasts.csv'), row.names = FALSE)
 
 f <- read.csv(file.path(lake_directory, 'all_obs_fcasts.csv'))
 
+##########################################################################
+# reliability diagrams
+rd <- f %>% 
+  select(reference_datetime, datetime, depth, model_id, variable, horizon,
+         observation, mean, sd)
+
+breaks <- seq(0, 1, by = 0.10)
+
+for(i in 1:nrow(rd)){
+  for(j in 2:length(breaks)){
+    # subset to forecasts within each break range of the distribution
+    prob <- qnorm(breaks[j], rd$mean[i], rd$sd[i]) 
+    if(prob)
+  }
+}
+
+p <- rd %>% 
+  mutate()
 
 ##########################################################################
 library(scoringRules)
@@ -71,6 +89,9 @@ f <- f %>%
          mean_mgL_C = ifelse(variable=='oxygen', mean*32/1000, mean),
          crps2 = crps(observation, family = "normal", mean = mean, sd = sd),
          crps2_mgL = ifelse(variable=='oxygen', crps2*32/1000, crps2))
+
+mean_score_temp <- mean(f$crps2[f$variable=='temperature'])
+mean_score_oxy <- mean(f$crps2[f$variable=='oxygen'])
 
 ggplot(f[f$model_id!='all_UC',], aes(x = as.Date(datetime), y = crps2_mgL)) +
   geom_point(aes(color = model_id)) +
