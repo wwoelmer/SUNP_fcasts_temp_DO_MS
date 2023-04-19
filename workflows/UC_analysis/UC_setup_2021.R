2f097bb67162aec6a002f2998855cec262817ab7
Whitney Marie Woelmer <wwoelmer@tc001.cm.cluster>
2023-04-19 09:54:36 -0400

    new indx

--- workflows/UC_analysis/UC_setup_2021.R
+++ workflows/UC_analysis/UC_setup_2021.R
@@ -145,7 +145,7 @@ for(i in 1:length(UC_names)){
   
 }
 
-starting_index <- 255
+starting_index <- 376
 set.seed(24)
 # index 415 failed, only 16-day forecasts for some ensembles on 2022-08-09
 # no NOAA forecasts on 2022-08-10
