57bfc84f6c65f9c7e9eb798c56c5a4ca7207f2d8
Whitney Marie Woelmer <wwoelmer@tc078.cm.cluster>
2023-10-18 01:01:16 -0400

    update zenodo urls, add null scores and targets

--- workflows/SUNP_fcasts_temp_DO/01_download_forecasts.R
+++ workflows/SUNP_fcasts_temp_DO/01_download_forecasts.R
@@ -6,34 +6,40 @@ site_id <- "sunp"
 
 ##### set up directories
 # for scores
-dest <- file.path(lake_directory, 'scores')
-dir.create(dest)
-
-dest <- file.path(lake_directory, 'scores', site_id)
-dir.create(dest)
-
 dest <- file.path(lake_directory, 'scores', site_id, sim_name)
-dir.create(dest)
+dir.create(dest, recursive = TRUE)
 
 # for forecasts
-dest <- file.path(getwd(), 'forecasts', site_id)
-dir.create(dest)
-
 dest <- file.path(getwd(), 'forecasts', site_id, sim_name)
-dir.create(dest)
+dir.create(dest, recursive = TRUE)
 
 
 ##### download scores files
-scores <- "https://zenodo.org/record/8212703/files/scores.zip?download=1"
+scores <- "https://zenodo.org/records/10016133/files/scores.zip?download=1"
 scores_dest <- file.path(lake_directory, 'scores', site_id, sim_name, "scores.zip")
 download.file(url = scores, destfile = scores_dest)
 unzip(scores_dest, exdir = file.path(lake_directory, 'scores', site_id, sim_name))
 unlink(scores_dest)
 
+##### download scores files for climatology forecasts
+scores <- "https://zenodo.org/records/10016133/files/climatology_scores.csv?download=1"
+scores_dest <- file.path(lake_directory, 'scores', site_id, "climatology_scores.csv")
+download.file(url = scores, destfile = scores_dest)
+
+##### download scores files for random walk (aka persistence) forecasts
+scores <- "https://zenodo.org/records/10016133/files/RW_scores.csv?download=1"
+scores_dest <- file.path(lake_directory, 'scores', site_id, "RW_scores.csv")
+download.file(url = scores, destfile = scores_dest)
+
+##### download target observations
+targets <- "https://zenodo.org/records/10016133/files/sunp-targets-insitu.csv?download=1"
+scores_dest <- file.path(lake_directory, 'targets', site_id, sim_name, "sunp-targets-insitu.csv")
+download.file(url = scores, destfile = scores_dest)
+
 ##### download forecast files
 ###### NOTE: this takes a long time! #######
 
-fcasts <- "https://zenodo.org/record/8212703/files/forecasts.zip?download=1"
+fcasts <- "https://zenodo.org/records/10016133/files/forecasts.zip?download=1"
 fcast_dest <- file.path(lake_directory, 'forecasts', site_id, sim_name, "fcasts.zip")
 download.file(url = fcasts, destfile = fcast_dest,
               method = "curl")
